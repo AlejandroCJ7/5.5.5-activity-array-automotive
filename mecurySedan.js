a8764402f5ca20776bb2c4294a012c34ae726712
Alex Castaneda <alex@Alexs-MacBook-Pro.local>
2022-12-21 21:46:21 -0800

    new array automotive

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,38 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+
+class Car extends Vehicle {
+    constructor(name, maximumPassengers, passenger, numberOfWheels, maximumSpeed, fuel) {
+      super(name, numberOfWheels, maximumSpeed, fuel);
+      this.maximumPassengers = maximumPassengers;
+      this.passenger = passenger;
+    }
+  
+    loadPassenger(num) {
+      if (this.passenger < this.maximumPassengers) {
+        this.passenger += num;
+        return true;
+      }
+      return false;
+    }
+  
+    start() {
+      if (this.fuel > 10) {
+        return true;
+      }
+      else return false;
+    }
+  
+    scheduleService(mileage) {
+      if (mileage > 30000) {
+        this.scheduleService = true;
+        return true;
+      }
+      return false;
+    }
+  }
+  
+  const A28Mercury = new Car('A28 Mercury', 5, 0, 4, 160, 10);
+  
